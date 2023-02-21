5c118ce5e127bbd1a0b51985ee09a9ca0db09a5a
Leonardo Dutra <leonardompdutra@gmail.com>
2023-02-21 01:05:07 -0300

    vpc ajustment

--- ci_deploy.sh
+++ ci_deploy.sh
@@ -5,13 +5,13 @@
 ../../terraform.exe plan -var-file=var.tfvars -lock=false
 ../../terraform.exe apply -var-file=var.tfvars -auto-approve
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 588338176709.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 34243243242342.dkr.ecr.us-east-1.amazonaws.com
 
 docker build -t devsecops/jenkins-master .
 
-docker tag devsecops/jenkins-master:latest 588338176709.dkr.ecr.us-east-1.amazonaws.com/bar:latest
+docker tag devsecops/jenkins-master:latest 31232132321321.dkr.ecr.us-east-1.amazonaws.com/bar:latest
 
-docker push 588338176709.dkr.ecr.us-east-1.amazonaws.com/bar:latest
+docker push 32322332131232.dkr.ecr.us-east-1.amazonaws.com/bar:latest
  588338176709.dkr.ecr.us-east-1.amazonaws.com/bar:latest
 
 cluster
